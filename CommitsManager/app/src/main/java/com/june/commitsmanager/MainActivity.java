88ad8a401bcd9d26346e2a9d3332752c8c3843ce
kimkyunghoon <byul2sol@naver.com>
2020-07-13 12:21:14 +0900

    Realtime Database 적용으로 프로젝트 재업로드

---
+++ CommitsManager/app/src/main/java/com/june/commitsmanager/MainActivity.java
@@ -0,0 +1,34 @@
+package com.june.commitsmanager;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button btnStart, btnExit;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnStart =(Button)findViewById(R.id.btnStart);
+        btnExit =(Button)findViewById(R.id.btnExit);
+        btnStart.setOnClickListener((View.OnClickListener) this);
+        btnExit.setOnClickListener((View.OnClickListener) this);
+    }
+
+    public void onClick(View v) {
+        if(v.getId()==R.id.btnStart) {
+            Intent intent = new Intent(getApplicationContext(), WelcomeActivity.class);
+            startActivity(intent);
+        }
+        else { // 종료 버튼 누를 시
+            finish();
+        }
+    }
+}
